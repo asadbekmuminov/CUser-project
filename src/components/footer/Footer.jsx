a9c81ebd42d751208a43b298998f56fd66c35ee9
asadbekmuminov <asadbekmuminov@gmail.com>
2023-09-20 14:36:30 +0500

    create-user-with-ract

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container container "></div>
+      <p>
+        Asadbek Muminov <a href="https://github.com/asadbekmuminov">Github</a>
+      </p>
+    </div>
+  );
+}
+
+export default footer;
